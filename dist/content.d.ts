3a734705592df798af8acbae83f2fd8eb8439d11
mkusaka <hinoshita1992@gmail.com>
2019-08-26 00:31:28 +0900

    add simple notifier

---
+++ dist/content.d.ts
@@ -0,0 +1,1 @@
+declare function notifyMe(message: any): void;
---
+++ dist/content.js
@@ -0,0 +1,20 @@
+window.addEventListener("error", (evt) => {
+    console.log("imhere");
+    notifyMe(evt.message);
+});
+function notifyMe(message) {
+    if (Notification.permission === "granted") {
+        new Notification(message);
+    }
+    else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function (permission) {
+            if (permission === "granted") {
+                new Notification(message);
+            }
+            else {
+                console.log(":cry:");
+            }
+        });
+    }
+}
+//# sourceMappingURL=content.js.map
